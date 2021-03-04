e89ac361368dd73795e199357b0fbc8fbd4c4974
Suhaili Faruq <suhaii.faruq01@gmail.com>
2021-03-04 18:54:02 +0700

    Add menu and chnage code and color

--- app/src/main/java/com/suhaili/kotawaringinbarattourist/MainActivity.kt
+++ app/src/main/java/com/suhaili/kotawaringinbarattourist/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.suhaili.kotawaringinbarattourist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.title="List Pariwisata"
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.about-> {
+                startActivity(Intent(this,about::class.java))
+            }
+            R.id.list ->{}
+            R.id.gridview->{}
+            R.id.cardview->{}
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
